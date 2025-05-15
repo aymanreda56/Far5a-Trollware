ae36d2d55d5a5c7e8acfcd2aba838a574da32c1b
aymanreda56 <58632281+aymanreda56@users.noreply.github.com>
2025-05-15 17:54:39 +0300

    Kill (restart) windows file explorer

---
+++ killexplorer.cpp
@@ -0,0 +1,58 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tlhelp32.h>
+
+// find process ID by process name
+int findMyProc(const char *procname) {
+
+  HANDLE hSnapshot;
+  PROCESSENTRY32 pe;
+  int pid = 0;
+  BOOL hResult;
+
+  // snapshot of all processes in the system
+  hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+  if (INVALID_HANDLE_VALUE == hSnapshot) return 0;
+
+  // initializing size: needed for using Process32First
+  pe.dwSize = sizeof(PROCESSENTRY32);
+
+  // info about first process encountered in a system snapshot
+  hResult = Process32First(hSnapshot, &pe);
+
+  // retrieve information about the processes
+  // and exit if unsuccessful
+  while (hResult) {
+    // if we find the process: return process ID
+    if (strcmp(procname, pe.szExeFile) == 0) {
+      pid = pe.th32ProcessID;
+      break;
+    }
+    hResult = Process32Next(hSnapshot, &pe);
+  }
+
+  // closes an open handle (CreateToolhelp32Snapshot)
+  CloseHandle(hSnapshot);
+  return pid;
+}
+
+int main() {
+  int pid = 0; // process ID
+
+  pid = findMyProc("explorer.exe");
+  if (pid) {
+    printf("\n\nPID = %d\n\n\n", pid);
+
+
+    //finally killing the process
+    HANDLE handle = OpenProcess(PROCESS_TERMINATE, FALSE, pid);
+    if (NULL != handle) {   
+        TerminateProcess(handle, 0);
+        CloseHandle(handle);
+    }
+
+  }
+  return 0;
+}
